5ca8eeccf13dde43a39011405d4708ce4353d586
Celray James CHAWANDA <celray.chawanda@outlook.com>
2025-12-23 13:38:05 -0600

    updates for compiling on arch/manjaro

--- QSWATPlus/setuppyx.py
+++ QSWATPlus/setuppyx.py
@@ -1,40 +1,44 @@
 from distutils.core import setup
-    
 from Cython.Build import cythonize  # @UnresolvedImport
 import os
 import numpy
+
 if 'QSWAT_PROJECT' in os.environ and 'Linux' in os.environ['QSWAT_PROJECT']:
     includePath = '/usr/include/python3.'
-    numpyInclude = numpy.get_include()
     sep = ':'
     is32 = '_32' in os.environ['QSWAT_PROJECT']
 elif 'QSWAT_PROJECT' in os.environ and 'Mac' in os.environ['QSWAT_PROJECT']:
     includePath = '/usr/local/opt/python@3.9/Frameworks/Python.framework/Versions/3.9/include/python3.9'
-    numpyInclude = '/usr/local/lib/python3.9/site-packages/numpy/core/include'
     sep = ':'
     is32 = '_32' in os.environ['QSWAT_PROJECT']
 else:
-    includePath = os.environ['OSGEO4W_ROOT'] + r'/apps/Python37/include'
-    numpyInclude = numpy.get_include()
+    # Windows
+    osgeo_root = os.environ.get('OSGEO4W_ROOT', '')
+    includePath = osgeo_root + r'/apps/Python37/include'
     sep = ';'
     is32 = False
+
+numpyInclude = numpy.get_include()
+
 if 'INCLUDE' in os.environ:
-    os.environ['INCLUDE'] = os.environ['INCLUDE'] + sep + includePath + sep + numpyInclude 
+    os.environ['INCLUDE'] = os.environ['INCLUDE'] + sep + includePath + sep + numpyInclude
 else:
     os.environ['INCLUDE'] = includePath + sep + numpyInclude
-    
+
 print('include path is {0}'.format(os.environ['INCLUDE']))
 
-# NB for Linux also had to 
-#sudo ln -s  /usr/lib/python3/dist-packages/numpy/core/include/numpy /usr/include/numpy
-# for it to find numpy/arrayobject.h
+extensions = cythonize('*.pyx', include_path = [os.environ['INCLUDE']])
+
+for ext in extensions:
+    if ext.include_dirs is None:
+        ext.include_dirs = []
+    ext.include_dirs.append(numpyInclude)
 
 if is32:
-    # only run cythonize to get .c files from .pyx
-    cythonize('*.pyx', include_path = [os.environ['INCLUDE']])
+    pass
 else:
     setup(
         name = "pyxes",
-        package_dir = {'QSWATPlus': ''}, 
-        ext_modules = cythonize('*.pyx', include_path = [os.environ['INCLUDE']]),
+        package_dir = {'QSWATPlus': ''},
+        ext_modules = extensions,
     )
